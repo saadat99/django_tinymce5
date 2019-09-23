fbe3cdb028a67392882f047476654c21c27e929d
Saadat Esmaeily <saadat.esmaeily@gmail.com>
2019-09-24 02:13:25 +0330

    Added TinyMCE

--- flowers_django/settings.py
+++ flowers_django/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main',
+    'tinymce',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'static/'
+
+# TINYMCE_JS_URL = STATIC_URL + 'tinymce/tinymce.min.js'
+# TINYMCE_DEFAULT_CONFIG = {
+#     'theme': "silver",
+# }
--- main/models.py
+++ main/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from datetime import datetime
+from django.conf import settings
+from django.contrib.auth.models import User
+
+from tinymce.models import HTMLField
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    body = HTMLField()
+    created_at = models.DateTimeField(default=datetime.now, blank=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, default=1, on_delete=models.CASCADE)
+    # 'Post object' title fix
+    def __str__(self):
+        return self.title
+
